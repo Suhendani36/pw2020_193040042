60c0f0ec9771b3432d29da5524ac1c3f2ccaa181
Suhendani42 <dani.fs163@gmail.com>
2020-04-13 21:48:54 +0700

    Up

---
+++ praktikum/P5_PW_193040042/latihan5a/index.php
@@ -0,0 +1,57 @@
+<?php
+$conn = mysqli_connect("localhost","root","") or die("Koneksi ke DB gagal");
+
+mysqli_select_db($conn,"tubes_193040042") or die("Database Salah");
+
+$result = mysqli_query($conn, "SELECT * FROM makanan");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        table{
+            width: 550px;
+            margin:auto;
+            background-color:#ffffe0;
+            color:black;
+            text-align: center;
+        }
+    
+    </style>
+</head>
+<body>
+    <table border="1" cellpadding="7" cellspacing="0">
+    
+    <tr>
+        <th>NO.</th>
+        <th>Gambar</th>
+        <th>Nama</th>
+        <th>Bahan</th>
+        <th>Daerah Asal</th>
+        <th>Manfaat</th>
+    </tr>
+    <?php $no = 1?>
+    <?php while($row = mysqli_fetch_assoc($result)) : ?>
+        <tr>
+            <td><?= $no?></td>
+            <td><img src="assets/img/<?= $row['gambar']?>"></td>
+            <td><?= $row['nama']?></td>
+            <td><?= $row['bahan']?></td>
+            <td><?= $row['daerah_asal']?></td>
+            <td><?= $row['manfaat']?></td>
+        </tr>
+    <?php 
+        $no++;
+    ?>
+    <?php endwhile; ?>
+    </table>
+</body>
+</html>
+
+
+
---
+++ praktikum/P5_PW_193040042/latihan5c/index.php
@@ -0,0 +1,49 @@
+<?php
+require 'php/function.php';
+
+$makanan = query("SELECT * FROM makanan");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+    .container{
+        width:500px;
+        margin-top:40px;
+        border:1px solid gray;
+        padding: 20px 20px 20px 20px;
+    }
+    h5{
+        text-decoration: underline;
+    }
+    </style>
+</head>
+<body>
+        <div class="container">
+        <h5 class="list-group-item active">
+        List Makanan
+        </h5>
+            <?php foreach($makanan as $mkn) :?>
+                <div class="list-group">
+                    
+                         <a href="php/detail.php?id=<?= $mkn['id']?>" class="list-group-item list-group-item-action">
+                            <?= $mkn["nama"]?>       
+                        </a>
+                    
+                </div>
+            <?php endforeach; ?>
+        </div>
+    
+   
+</body>
+</html>
+
+
+>
+
+
